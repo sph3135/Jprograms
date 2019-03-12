6208adc38b5919398cba7ef2529bb28d2a7ad149
sphn3222 <sudeepph360@gmail.com>
2019-03-12 10:58:41 +0530

    2nd Jprogram

---
+++ add.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class add{
+
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int a,b,c=0;
+        System.out.println("Enter The First Number :");
+        a=input.nextInt();
+        System.out.println("Enter The Second Number :");
+        b=input.nextInt();
+        c=a+b;
+        System.out.println("Sum="+c);;
+
+    }
+        
+    
+    
+}
